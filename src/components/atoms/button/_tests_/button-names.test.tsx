31bd1188b61bd270e3c520b0e32468c0b9ce6d5b
noviceGuru <amin.arabi@gmail.com>
2023-06-12 05:48:00 +0200

    jest was added and buttons names test was run successfully

---
+++ src/components/atoms/button/_tests_/button-names.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react'
+
+import Button from "../button"
+
+import { ButtonTypes } from '@/features/types/todos'
+
+const buttonsTypesAndExpectedTexts: { type: ButtonTypes, text: string }[] = [
+	{
+		type: 'add',
+		text: 'Add new'
+	},
+	{
+		type: 'delete',
+		text: 'Delete'
+	},
+	{
+		type: 'save',
+		text: 'Save'
+	},
+	{
+		type: 'discard',
+		text: 'Discard'
+	},
+	{
+		type: 'edit',
+		text: 'Edit'
+	}
+]
+
+buttonsTypesAndExpectedTexts.forEach(typeAndName => {
+	it('renders the add button with the correct name', () => {
+		render(
+			<Button
+				type={typeAndName.type}
+			/>)
+		const button = screen.getByRole('button')
+
+		expect(button.textContent).toBe(typeAndName.text)
+	})
+})
