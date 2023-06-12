0b4b6fca368d9b89756fdbc814aad2d400abd4ee
noviceGuru <amin.arabi@gmail.com>
2023-06-12 07:43:56 +0200

    title tests and snapshot created

---
+++ src/components/atoms/title/_tests_/title.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react'
+
+import Title from '../title'
+
+it('renderes title correctly with the correct looks', () => {
+
+	const tableRow = document.createElement('tr')
+	render(
+		<Title
+			title='dummy text title to test'
+		/>
+		, { container: document.body.appendChild(tableRow) })
+	
+	const title = screen.getByRole('columnheader')
+	expect(title).toBe('dummy text title to test')
+	expect(title).toMatchSnapshot()
+})
