fb4b38a16ac864cedce25abadba3a8146f4854ef
noviceGuru <amin.arabi@gmail.com>
2023-06-12 06:49:07 +0200

    button action test added

---
+++ src/components/atoms/button/_tests_/button-action.test.tsx
@@ -0,0 +1,17 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+
+import Button from '../button'
+
+it('onClick performs correctly the dummy function passed to the button', () => {
+	const logSpy = jest.spyOn(global.console, 'log')
+	
+	render(<Button
+		type='add'
+		onClick={() => console.log('The button is logging the onclick function')}
+	/>)
+	
+	const button = screen.getByRole('button', { name: /Add new/i })
+	fireEvent.click(button)
+
+	expect(logSpy).toHaveBeenCalledWith('The button is logging the onclick function')
+})
