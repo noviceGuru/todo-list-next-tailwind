51d680cea1cf6ffd8dc36b62d32e649994317429
noviceGuru <amin.arabi@gmail.com>
2023-06-23 13:59:57 +0200

    test added
    Once an edit button clicked, other row(s) will not change

--- src/components/molecules/table/_tests_/table-functions.test.tsx
+++ src/components/molecules/table/_tests_/table-functions.test.tsx
@@ -57,3 +57,54 @@ test('by clicking edit, cell becomes an input, save and discard buttons appear i
 	expect(row1).not.toContainEqual(edit_button_1)
 	expect(row1).not.toContainEqual(delete_button_1)
 })
+
+test('by clicking edit, the other row does not change', () => {
+	render(<Table
+		initialData={initialData}
+	/>)
+
+	const table = screen.getByRole('table')
+	const row1 = within(table).getByRole('row', {
+		name: 'write tests Delete Edit'
+	})
+
+	const edit_button_1 = within(row1).getByRole('button', {
+		name: /edit/i
+	})
+
+	fireEvent.click(edit_button_1)
+
+	const row2 = screen.getByRole('row', {
+		name: /make the storybook delete edit/i
+	})
+
+	const textCell = within(row2).getByRole('cell', {
+		name: /make the storybook/i
+	})
+
+	const edit_button_2 = within(row2).getByRole('button', {
+		name: /edit/i
+	})
+	
+	const delete_button_2 = within(row2).getByRole('button', {
+		name: /delete/i
+	})
+
+	const input2 = within(row2).queryByRole('textbox')
+
+	const discard_button2 = within(row2).queryByRole('button', {
+		name: /discard/i
+	})
+
+	const save_button2 = within(row2).queryByRole('button', {
+		name: /save/i
+	})
+
+	expect(row2).toContainElement(textCell)
+	expect(row2).toContainElement(edit_button_2)
+	expect(row2).toContainElement(delete_button_2)
+
+	expect(row2).not.toContainEqual(input2)
+	expect(row2).not.toContainEqual(discard_button2)
+	expect(row2).not.toContainEqual(save_button2)
+})
