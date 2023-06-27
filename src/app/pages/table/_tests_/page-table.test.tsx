2e36907e0c0d377601f26b8dd131e39280e61879
noviceGuru <amin.arabi@gmail.com>
2023-06-27 15:42:05 +0200

    A test was created in server component page which with the fetched data renders the table.
    tests if the rows render correctly

--- src/app/pages/table/_tests_/page-table.test.tsx
+++ src/app/pages/table/_tests_/page-table.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, within } from "@testing-library/react"
 import '@testing-library/jest-dom'
 
 import { mswServer } from "@/utils/testApis/server"
-import { initial_fetchTodos_success } from "@/utils/testApis/testApiHandlers"
+import { initalTodos, initial_fetchTodos_success } from "@/utils/testApis/testApiHandlers"
 import TablePage from "../page"
 
 beforeEach(() => mswServer.listen())
@@ -15,6 +15,20 @@ test('after successful fetch, renders the table correctly', async () => {
 	render(
 		await (async () => await TablePage())()
 	)
-
-	expect(screen.getByRole('table')).toBeInTheDocument()
+	
+	const pageTable = screen.getByRole('table')
+	
+	const cell1 = within(pageTable).getByRole('cell', {
+		name: initalTodos[0].task
+	})
+	const cell1TextSpan = within(cell1).getByText(initalTodos[0].task)
+	
+	const cell2 = within(pageTable).getByRole('cell', {
+		name: initalTodos[1].task
+	})
+	const cell2TextSpan = within(cell2).getByText(initalTodos[1].task)
+	
+	expect(pageTable).toBeInTheDocument()
+	expect(cell1TextSpan.innerHTML).toBe(initalTodos[0].task)
+	expect(cell2TextSpan.innerHTML).toBe(initalTodos[1].task)
 })
