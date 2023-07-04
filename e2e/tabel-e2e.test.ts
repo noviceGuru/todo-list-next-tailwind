957c34e429ba6ebf31c0f295d1cc95cef0cc2b66
noviceGuru <amin.arabi@gmail.com>
2023-07-04 23:22:21 +0200

    deleting a task was added to e2e test

--- e2e/tabel-e2e.test.ts
+++ e2e/tabel-e2e.test.ts
@@ -16,7 +16,7 @@ test('Adds a task successfully', async ({ page }) => {
 		}
 	})
 
-	const addButton = page.getByRole('button', { name: /add new/i })
+	const addButton = await page.getByRole('button', { name: /add new/i })
 	await expect(addButton).toBeVisible()
 
 	addButton.click()
@@ -37,4 +37,34 @@ test('Adds a task successfully', async ({ page }) => {
 	const cell = await page.getByRole('cell', { name: new RegExp(testTodos.todosAfterOneRowAdded[2].task, 'i') })
 
 	await expect(cell).toBeVisible()
+})
+
+test('Deletes a task successfully', async ({ page }) => {
+	await page.goto('http://localhost:3000/pages/table')
+
+	await page.route('http://localhost:3001/todos', route => {
+		if (route.request().method() === 'GET') {
+			return route.fulfill({
+				body: JSON.stringify(testTodos.todosAfterDeleteTodo1)
+			})
+		}
+	})
+	
+	await page.route('http://localhost:3001/todos/*', route => {
+		if (route.request().method() === 'DELETE') {
+			return route.fulfill({
+				body: JSON.stringify(testTodos.todosAfterDeleteTodo1)
+			})
+		}
+	})
+
+	const delete_button1 = await page.getByRole("button", { name: /delete/i }).first()
+	await expect(await delete_button1).toBeVisible()
+	
+	await delete_button1.click()
+
+	const cell1 = await page.getByRole('table')
+		.filter({ hasText: testTodos.initalTodos[0].task })
+
+	await expect(cell1).not.toBeVisible()
 })
