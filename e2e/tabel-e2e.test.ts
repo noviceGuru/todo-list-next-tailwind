04cff3567cacc4dd4f558ddd64b0646437c8022f
noviceGuru <amin.arabi@gmail.com>
2023-07-04 17:57:40 +0200

    API calls were intercepted and mocked in table-e2e.test.ts

--- e2e/tabel-e2e.test.ts
+++ e2e/tabel-e2e.test.ts
@@ -1,7 +1,20 @@
 import { test, expect } from '@playwright/test'
+import { testTodos } from '../src/utils/testApis/testApiHandlers'
 
 test('Adds a task successfully', async ({ page }) => {
 	await page.goto('http://localhost:3000/pages/table')
+
+	await page.route('http://localhost:3001/todos', route => {
+		if (route.request().method() === 'GET') {
+			return route.fulfill({
+				body: JSON.stringify(testTodos.todosAfterOneRowAdded)
+			})
+		} else if (route.request().method() === 'POST') {
+			return route.fulfill({
+				body: JSON.stringify({})
+			})
+		}
+	})
 
 	const addButton = page.getByRole('button', { name: /add new/i })
 	await expect(addButton).toBeVisible()
@@ -13,7 +26,7 @@ test('Adds a task successfully', async ({ page }) => {
 	await expect(inputTextArea).toBeVisible()
 
 	await inputTextArea.clear()
-	await inputTextArea.fill(`world of wonder`)
+	await inputTextArea.fill(testTodos.todosAfterOneRowAdded[2].task)
 
 	const saveButton = await page.getByRole('button', { name: /save/i })
 
@@ -21,7 +34,7 @@ test('Adds a task successfully', async ({ page }) => {
 
 	await saveButton.click()
 
-	const cell = await page.getByRole('cell', {name: /world of wonder/i })
-	
+	const cell = await page.getByRole('cell', { name: new RegExp(testTodos.todosAfterOneRowAdded[2].task, 'i') })
+
 	await expect(cell).toBeVisible()
 })
