8c859735325f71cc21adc5214a80abd338758fc6
noviceGuru <amin.arabi@gmail.com>
2023-07-27 05:55:09 +0200

    warning resolved

--- src/components/atoms/link-button/_tests_/link-button.test.tsx
+++ src/components/atoms/link-button/_tests_/link-button.test.tsx
@@ -1,17 +1,18 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import '@testing-library/jest-dom'
 
 import LinkButton from "../link-button"
 
-test('link-button renders correctly, reproduces correct link and text', () => {
+test('link-button renders correctly, reproduces correct link and text', async () => {
 	render(<LinkButton
 		text="link button dummy text"
 		to="pages/a-dummy-url"
 	/>)
 
-	const linkButton = screen.getByRole('link', {
+	const linkButton = await waitFor(() => screen.getByRole('link', {
 		name: /link button dummy text/i
-	  })
+	})
+	)
 
 	expect(linkButton).toBeInTheDocument()
 	expect(linkButton.textContent).toBe("link button dummy text")
